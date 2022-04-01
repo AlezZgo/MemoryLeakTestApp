d3b99890025e303eadc638ba79da3b5037ebe53c
Alexander <alez.grante@gmail.com>
2022-04-01 21:47:52 +0300

    Simulated memory leaks with no alarm in profiler

--- app/src/main/java/com/example/memoryleaktestapp/MainActivity.kt
+++ app/src/main/java/com/example/memoryleaktestapp/MainActivity.kt
@@ -2,14 +2,31 @@ package com.example.memoryleaktestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.memoryleaktestapp.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MyViewModel
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         viewModel = MyViewModel()
+
+        binding.button.setOnClickListener {
+            viewModel.list.add(this)
+        }
+
+
+
+        setContentView(binding.root)
+
     }
+
+
+
+
 }
--- app/src/main/java/com/example/memoryleaktestapp/MyViewModel.kt
+++ app/src/main/java/com/example/memoryleaktestapp/MyViewModel.kt
@@ -1,12 +1,14 @@
 package com.example.memoryleaktestapp
 
+import android.app.Activity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MyViewModel : ViewModel() {
-    
 
-    init {
+    val ld = MutableLiveData<Int>()
 
-    }
+    val list = mutableListOf<Activity>()
 
 }
